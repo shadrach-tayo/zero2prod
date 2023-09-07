c8c9dd2db534839cb8256c01e3e9f3665ab49138
shadrach-tayo <shadrachtemitayo@gmail.com>
2023-09-07 09:28:20 +0200

    add first integration test

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,6 @@
-use std::{env, io};
-use actix_web::{middleware, web, App, HttpRequest, HttpServer, Responder, HttpResponse};
+use zero2prod::run;
 
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("world");
-    format!("Hellos {}!", name)
-}
-
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
-}
 #[tokio::main]
-async fn main() -> Result<(), io::Error> {
-    let port = env::args().nth(1).unwrap_or("8000".to_string());
-    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
-    log::info!("starting server at http://localhost:{}", port);
-
-    HttpServer::new(move || {
-        App::new()
-            .wrap(middleware::Logger::default())
-            .route("/", web::get().to(greet))
-            .route("/{name}", web::get().to(greet))
-            .route("/health_check", web::get().to(health_check))
-    })
-        .bind(("127.0.0.1", port.parse().unwrap()))?
-        .run()
-        .await
+async fn main() -> Result<(), std::io::Error> {
+        run()?.await
 }
